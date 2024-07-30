35236396e110b7a04c52bd9a85c25bca3a1e2e3d
RomanKunal <0221csds107@niet.co.in>
2024-07-30 23:57:59 +0530

    committed

--- Revision/Base1.java
+++ Revision/Base1.java
@@ -2,10 +2,24 @@ package Revision;
 import java.util.*;
 public class Base1 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int res=a+5;
-        System.out.println(res);
-        sc.close();// this is used to remove the sc as it acts as garbage collector
+//        Scanner sc=new Scanner(System.in);
+//        int a=sc.nextInt();
+//        int res=a+5;
+//        System.out.println(res);
+//        sc.close();// this is used to remove the sc as it acts as garbage collector
+
+        int a=5;
+        int b=10;
+        int max=0;
+        /*if(a>b){
+            max=a;
+        }
+        else{
+            max=b;
+        }*/
+
+        max=a>b?a:b;
+        System.out.println(max);
+
     }
 }
