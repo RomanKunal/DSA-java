08ad966f3d142e39725fc8b4c0ad1e32e12531a3
RomanKunal <0221csds107@niet.co.in>
2024-09-08 14:04:15 +0530

    committed

---
+++ Array/Max_Product_SubArray.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class Max_Product_SubArray {
+    public static void main(String[] args) {
+
+    }
+    public int maxProduct(int[] nums) {
+        int leftProduct = 1; int rightProduct = 1; int ans = Integer. MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            if(leftProduct == 0){
+                leftProduct =1;
+            }
+            if(rightProduct == 0){
+                rightProduct =1;
+            }
+            leftProduct = leftProduct * nums[i];
+            rightProduct = rightProduct * nums[nums.length-1-i];
+            ans = Math.max(ans,Math.max(leftProduct,rightProduct));
+
+        }
+        return ans;
+    }
+}
