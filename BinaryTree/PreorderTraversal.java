72bbadff0bbdffa92d97408c3fbd4b985723958b
RomanKunal <0221csds107@niet.co.in>
2024-10-05 12:43:43 +0530

    committed

--- BinaryTree/PreorderTraversal.java
+++ BinaryTree/PreorderTraversal.java
@@ -1,33 +1,32 @@
 package BinaryTree;
-import java.util.*;
-public class InorderTraversal {
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PreorderTraversal {
     public static void main(String[] args) {
-        Node root=new Node(1);
+        Node root = new Node(1);
         root.left = new Node(2);
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
-        List<Integer> result=inOrder(root);
-        System.out.print("Inorder Traversal: ");
-        // Output each value in the
-        // inorder traversal result
+        List<Integer> result=preOrder(root);
         for (int val : result) {
             System.out.print(val + " ");
         }
         System.out.println();
-
     }
-    static void inorder(Node root,List<Integer> arr){
+    static void preorder(Node root, List<Integer> arr){
         if(root==null){
             return;
         }
-        inorder(root.left,arr);
         arr.add(root.data);
-        inorder(root.right,arr);
+        preorder(root.left,arr);
+        preorder(root.right,arr);
     }
-    static List<Integer> inOrder(Node root){
+    static List<Integer> preOrder(Node root){
         List<Integer> arr=new ArrayList<>();
-        inorder(root,arr);
+        preorder(root,arr);
         return arr;
     }
 }
