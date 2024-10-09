cac7e38c33d1a886870599318ae5a45b7472a037
RomanKunal <0221csds107@niet.co.in>
2024-10-10 00:13:14 +0530

    committed

--- OOPS/Abstract1.java
+++ OOPS/Abstract1.java
@@ -1,7 +1,25 @@
 package OOPS;
 
+abstract class Bank{
+    abstract int getroi();
+}
+class Pnb extends Bank{
+    @Override
+    int getroi() {
+        return 14;
+    }
+}
+class Icici extends Bank{
+    @Override
+    int getroi(){
+        return 15;
+    }
+}
 public class Abstract1 {
     public static void main(String[] args) {
-
+        Pnb p=new Pnb();
+        System.out.println(p.getroi());
+        Icici i=new Icici();
+        System.out.println(i.getroi());
     }
 }
