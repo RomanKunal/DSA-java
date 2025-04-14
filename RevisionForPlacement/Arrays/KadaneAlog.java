165d6f5a2deea0151cca6f1444a80063e7a9b359
RomanKunal <0221csds107@niet.co.in>
2025-04-14 23:53:08 +0530

    committed

--- RevisionForPlacement/Arrays/KadaneAlog.java
+++ RevisionForPlacement/Arrays/KadaneAlog.java
@@ -4,18 +4,43 @@ public class KadaneAlog {
     public static void main(String[] args) {
 
     }
-    static long check(int[] arr){
-        long maxi=Long.MIN_VALUE;
-        long sum=0;
-        for (int i = 0; i <arr.length ; i++) {
-            sum+=arr[i];
-            if(sum>maxi){
-                maxi=sum;
+//    static long check(int[] arr){
+//        long maxi=Long.MIN_VALUE;
+//        long sum=0;
+//        for (int i = 0; i <arr.length ; i++) {
+//            sum+=arr[i];
+//            if(sum>maxi){
+//                maxi=sum;
+//            }
+//            if(sum<0){
+//                sum=0;
+//            }
+//        }
+//        return maxi;
+//    }
+    //for contiguos sum
+    static int maxSubarraySum(int[] arr) {
+        int maxSum = arr[0];
+        int currentSum = arr[0];
+
+        int start = 0;       // start of current subarray
+        int tempStart = 0;   // potential new start
+        int end = 0;         // end of max subarray
+
+        for (int i = 1; i < arr.length; i++) {
+            if (currentSum < 0) {
+                currentSum = arr[i];
+                tempStart = i;
+            } else {
+                currentSum += arr[i];
             }
-            if(sum<0){
-                sum=0;
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+                start = tempStart;
+                end = i;
             }
         }
-        return maxi;
+        return maxSum;
     }
 }
