8e2906557ace207a7c38e1908c1b11b7e473a56d
RomanKunal <0221csds107@niet.co.in>
2025-05-08 13:08:25 +0530

    committed

---
+++ RevisionForPlacement/Arrays/ProductSumExceptSelf.java
@@ -0,0 +1,22 @@
+package RevisionForPlacement.Arrays;
+
+import java.util.Arrays;
+
+public class ProductSumExceptSelf {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int[] res = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            int product = 1;
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j) {
+                    product *= arr[j];
+                }
+            }
+            res[i] = product;
+        }
+
+        System.out.println(Arrays.toString(res));
+    }
+}
